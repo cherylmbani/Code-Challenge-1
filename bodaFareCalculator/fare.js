106a48b3b632d6f19bc1927a41b1e399d8565c05
Cheryl Mbani <mbanicheryl45@gmail.com>
2025-06-06 19:14:20 +0300

    initial commit

---
+++ bodaFareCalculator/fare.js
@@ -0,0 +1,8 @@
+function calculateBodaFare(distanceInKM){
+    const baseFare = 50; //KES
+    const chargePerKm = 15; // KES
+    const distanceFare = distanceInKM * chargePerKm;
+    const totalFare = baseFare + distanceFare;
+
+
+}
