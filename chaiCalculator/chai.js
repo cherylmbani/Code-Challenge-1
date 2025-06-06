b934270e1ca8bea913db96b33f31caf6969dd3cb
Cheryl Mbani <mbanicheryl45@gmail.com>
2025-06-06 18:14:46 +0300

    declaring ingredient varibles for chai

---
+++ chaiCalculator/chai.js
@@ -0,0 +1,7 @@
+function calculateChaiIngredients(numberOfCups){
+    const water = 200; //in ml
+    const milk = 50; // in ml
+    const teaLeaves = 1 // tablespoons
+    const sugar = 2 // tablespoons
+
+}
